d16e4ad7628f711c0761b1a56d7a2f0d1ea56e6a
RohithSriSharan <rohithsrishran16@gmail.com>
2023-06-19 03:22:08 +0100

    Home Page BP

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Footer from '../HeaderFooter/Footer'
 import './Home.css'
-import background from '../images/background.jpg'
-
-import { GiStairsGoal } from 'react-icons/gi';
 
 
+import {TbTargetArrow } from 'react-icons/tb';
+import { SiNounproject } from 'react-icons/si';
+import {TbBinaryTree2 } from 'react-icons/tb';
 const Home = () => {
   return (
     <div className='home'>
@@ -16,8 +16,10 @@ const Home = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
 
                   <div className='home-buttons'>
-                    <button>Resume</button>
-                    <a href='/connect'><button >Connect</button></a>
+                  <a href='/' > Resume</a>
+                    
+                    <a href='/connect'>Connect</a>
+                    
                 
                   </div>
               </div>
@@ -48,8 +50,7 @@ const Home = () => {
          I have included a few notable projects, information about my background, and skills that I have acquired in this journey. 
          I've also included a section on my goals both short-term and long term to give you a better understanding of where I'm headed in my career.
          </p>
-      <p>Scroll down or click on the tabs as you go along to see some of my work!</p>
-      <img src='https://static.wixstatic.com/media/a4c838_6005a0df54f446238b5c71cd8fc12695~mv2.png/v1/crop/x_358,y_955,w_622,h_471/fill/w_388,h_290,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Untitled%20design%20(5).png'></img>
+      <span>Scroll down or click on the tabs as you go along to see some of my work!</span>
       </div>
 
 
@@ -57,21 +58,21 @@ const Home = () => {
 
         <div className='highlights'>
               <div className='goal'>
-                <p><GiStairsGoal/></p>
+                <span><TbTargetArrow/></span>
                 <h3>Goals</h3>
                 <p>My plan on guiding individuals into their chosen career paths </p>
                 <button>Learn More</button>
               </div>
 
               <div className='goal'>
-                    <p><GiStairsGoal/></p>
+                    <span><SiNounproject/></span>
                     <h3>Projects</h3>
                     <p>Learn how I created " how to " video for  career changers ,Job strategies assessing Labour market and understanding career theories by applying to clients</p>
                     <button>Learn More</button>
               </div>
 
             <div className='goal'>
-              <p><GiStairsGoal/></p>
+              <span><TbBinaryTree2/></span>
               <h3>Skills</h3>
               <p>Combining technical proficiency, interpersonal  and analysing skills to effectively support career planning in individuals</p>  
               <button>Learn More</button>
--- src/HeaderFooter/Footer.jsx
+++ src/HeaderFooter/Footer.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import './Footer.css'
 import { BsLinkedin } from 'react-icons/bs';
 import { FaTwitter } from 'react-icons/fa';
-
+import { SiGmail } from 'react-icons/si';
 
 const Footer = () => {
    
   return (
     <div className='footer'>  
       <h1> Madhulika Bhattacharjee </h1>
-      <p> | Aspiring Career Mentor | Educator | Aiming to make a difference in people's lives. |</p>
-      
+      <p> | Aspiring Career Mentor | Educator |</p>
+      <p>| Aiming to make a difference in people's lives. |</p>
       <div className='footer-icon'>
-      <p><BsLinkedin/></p>
-      <p><FaTwitter/></p>
+        <span><BsLinkedin/></span>
+        <span><FaTwitter/></span>
+        <span><SiGmail/></span>
       </div>
       
       </div>
